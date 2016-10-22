ec5e91ec1a68e4e7ef54a8ff982cfbcfca9881f2
Kirk A. Jackson <jacksonkirka@bristosoft.com>
2016-10-22 14:48:57 -0400

    Added all python files to be tracked"

---
+++ userpwd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import uuid
+import hashlib
+
+def hashpwd(_pwd):
+        
+    '''
+    hashpwd hashes a password by NSA Secure Hash Algorithm 2 
+    sha256 algorithm and adds a uuid prefix salt.
+    '''
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() +\
+        _pwd.encode()).hexdigest() + ':' + salt
+
+passwd = raw_input('Please enter password to hash: ')
+
+print passwd
+print hashpwd(passwd)
