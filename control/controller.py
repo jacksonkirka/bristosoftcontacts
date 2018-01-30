13f7449dbfe1ee22cb8c7e366c0a0647453a0268
Kirk A. Jackson <jacksonkirka@bristosoft.com>
2018-01-29 23:40:11 -0500

    Updated restore_status_bar to properly restore the statusbar to red with user/db message.

--- control/controller.py
+++ control/controller.py
@@ -2832,7 +2832,7 @@ class Controller(QMainWindow, contactsmain.Ui_bristosoftContacts):
         '''
         self.contactsStatusBar.setStyleSheet("background-color: \
                                               rgb(230, 128, 128);")
-        # self.contactsStatusBar.removeWidget(self.conn_msg)
+        self.contactsStatusBar.removeWidget(self.conn_msg)
         self.conn_msg = QLabel(self._user+'@'+self._host+'/'+ self._db)
         self.contactsStatusBar.addWidget(self.conn_msg)
 
