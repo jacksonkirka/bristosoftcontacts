0a6671b50ab84593fac264912e55794c72417c53
Kirk Jackson <jacksonkirka@bristosoft.com>
2018-04-14 17:12:57 -0400

    Changed controller.Controller.db_close to use showMessage instead of add addWidget.

--- control/controller.py
+++ control/controller.py
@@ -2964,9 +2964,8 @@ class Controller(QMainWindow, contactsmain.Ui_bristosoftContacts):
                 self.contactsStatusBar.setStyleSheet("background-color: \
                                                       rgb(230, 128, 128);")
                 self.contactsStatusBar.removeWidget(self.conn_msg)
-                self.conn_msg = QLabel(self._host+
-                                          '/'+ self._db+'.')
-                self.contactsStatusBar.addWidget(self.conn_msg)
+                self.conn_msg = self._host+'/'+ self._db+'.'
+                self.contactsStatusBar.showMessage(self.conn_msg)
                 self._disconnected = True
                 
     def restore_status_bar(self):
