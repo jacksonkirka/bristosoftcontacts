c82f38a8b58624f058d0c62fc4f0e529b6532264
Kirk Jackson <jacksonkirka@bristosoft.com>
2018-05-16 01:19:43 -0400

    Added class InternetConnection internetPerformance and InternetReliability.

--- control/internet.py
+++ control/internet.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python2
+#
+# Copyright 2016 Kirk A Jackson DBA bristoSOFT all rights reserved.  All methods,
+# techniques, algorithms are confidential trade secrets under Ohio and U.S.
+# Federal law owned by bristoSOFT.
+#
+# Kirk A Jackson dba bristoSOFT
+# 4100 Executive Park Drive
+# Suite 11
+# Cincinnati, OH  45241
+# Phone (513) 401-9114
+# email jacksonkirka@bristosoft.com
+#
+# The trade name bristoSOFT is a registered trade name with the State of Ohio
+# document No. 201607803210.
+#
+'''
+This internet.py module provides testing of the existence, reliability and 
+performance of an internet connection.
+'''
 
+class InternetConnection:
+    pass
+
+class InternetPerformance:
+    pass
+
+class InternetReliability:
+    pass
