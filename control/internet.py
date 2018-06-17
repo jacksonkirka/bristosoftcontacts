ec8edd41778eddd7614f935176b1c417917dfacc
Kirk Jackson <jacksonkirka@bristosoft.com>
2018-06-16 22:53:10 -0400

    Updated internet.py module InternetPerformance class with research text which will later be deleted.

--- control/internet.py
+++ control/internet.py
@@ -23,6 +23,10 @@ import urllib2
 import socket
 
 class InternetConnection:
+    '''
+    InternetConnection is a class of resources used to
+    verify that an internet connection has been established.
+    '''
 
     def inet_request(_url):
         '''
@@ -50,5 +54,42 @@ class InternetReliability:
     pass
     
 class InternetPerformance:
+    '''
+    Internet "speed" actually consists of multiple factors, including bandwidth,
+    latency, and packet loss. What exactly are you trying to measure? If it's
+    bandwidth (data transfer speed in bits per second), then you should also
+    measure transfers of larger payloads, because the time to complete small
+    transfers will tend to be dominated by latency effects. If it's latency
+    (milliseconds for a round trip) that you are want to measure, then a tiny
+    packet (typically an ICMP echo request, commonly called a "ping") would be
+    more appropriate.
+
+    In addition, there are confounding issues:
+
+    Upload vs. download speed:
+        Residential Internet connections tend to significantly favour downloads,
+        since that's what most consumers care about.
+    Traffic shaping:
+        Some ISPs will throttle the bandwidth to penalize very large transfers,
+        so that smaller responses feel more responsive.
+    Server performance:
+        It does take a few milliseconds for Google's server to formulate the
+        response payload; that delay would be a more significant fraction of the
+        measured round-trip time for a short response than for a long response.
+    HTTP and HTTPS overhead:
+        You're considering only the size of the HTTP response payload. Since you're
+        using HTTPS, there are also multiple TCP round trips needed to perform the TLS
+        handshake, and a few kilobytes to transfer the certificates. The HTTP
+        headers could add another kilobyte or so. All of this is significant
+        when the payload is only ~10 kB short.
+    DNS overhead:
+        Resolving the google.com hostname to an IP address might involve a DNS 
+        lookup, which would add a few milliseconds. The time for that DNS lookup
+        is accounted for in your benchmark, but not the traffic.
+    Proximity:
+        The "Internet" is not one entity. Connectivity to various parts of the
+        Internet will have different performance characteristics.
+
+    '''
     pass
 
