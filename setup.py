d14818ad210320e2e6baf4b9c38f4886b27d6a7a
Kirk Jackson <jacksonkirka@bristosoft.org>
2019-09-16 21:50:02 -0400

    Misc mod to setup.py.

--- setup.py
+++ setup.py
@@ -24,8 +24,7 @@ from setuptools import setup, find_packages
 setup(
     name="bristoSOFTContacts",
     version="0.10a",
-    description='bristoSOFT Contacts(tm) is a group oriented contact managment \
-    system.',
+    description='bristoSOFT Contacts(tm) is a group oriented contacts managment system.',
     long_description='bristoSOFT Contacts is a group oreinted contact management \
     system.  Its stack is based MVC and includes Qt, Python and PostgreSQL. \
     Users can query groups of contacts by username and password.', 
